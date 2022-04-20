8cd7f8ee8ef01947f42705351c360f2167408b1d
Giovanna Toni <giovannaportotoni@gmail.com>
2022-04-20 20:11:54 -0300

    trying to solve igit pull problems

---
+++ tamojunto/tamojunto/Model/CreateThreadDTOModel.swift
@@ -0,0 +1,12 @@
+//
+//  CreateThreadDTOModel.swift
+//  tamojunto
+//
+//  Created by Gustavo Perbone on 12/04/22.
+//
+
+import Foundation
+
+struct CreateThreadDTO: Codable {
+    var title, content, subjectId: String
+}
